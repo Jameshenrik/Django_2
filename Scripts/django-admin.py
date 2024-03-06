4d9a3abecde560235e71a3adbf17cb122ce66797
James Middleton <jameshenrik@outlook.com>
2024-03-06 16:58:29 +0000

    initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\james\OneDrive\University\Year_2\WAD\Django_2\rangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
