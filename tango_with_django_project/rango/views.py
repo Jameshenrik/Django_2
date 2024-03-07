e4000faad85866fbe71107a132202ddf98e0b9d5
James Middleton <jameshenrik@outlook.com>
2024-03-07 18:06:32 +0000

    Add HTM: Hyperlink to index

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Clap along if you feel like happiness is for you")
+    return HttpResponse("Clap along if you feel like happiness is for you <a href='about'>About</a>")
+
+def about(request):
+    return HttpResponse("Clap along if you know that happiness is the truth")
